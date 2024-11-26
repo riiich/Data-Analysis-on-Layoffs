89dd54aaa919efb43cfaa908d4bb8ae004baa00f
Rich <horich21@gmail.com>
2024-11-26 08:05:14 -0800

    .

--- Exploratory Data Analysis.sql
+++ Exploratory Data Analysis.sql
@@ -5,7 +5,7 @@ FROM layoffs_staging2;
 
 /* 
 	Date range of layoffs
-	- The first recorded layoff in this dataset was in March 3, 2020 to March 6, 2023
+	- The first recorded layoff in this dataset was in March 3, 2020 to March 6, 2023.
 */
 SELECT MIN(date), MAX(date)
 FROM layoffs_staging2;
@@ -30,7 +30,7 @@ FROM layoffs_staging2;
 
 
 /*
-	Looks like the top tech companies had the most layoffs (Google, Meta, Microsoft, Amazon)
+	Looks like the top tech companies had the most layoffs (Google, Meta, Microsoft, Amazon).
 */
 SELECT company, total_laid_off, percentage_laid_off, date
 FROM layoffs_staging2
@@ -39,7 +39,7 @@ ORDER BY total_laid_off DESC;
 
 
 /* 
-	All the 
+	All the layoffs within each industry.
 */
 SELECT industry, SUM(total_laid_off)
 FROM layoffs_staging2
@@ -48,7 +48,7 @@ ORDER BY 2 DESC;
 
 
 /*
-	Sum of layoffs throughout the months and years
+	Sum of layoffs throughout the months and years.
 */
 SELECT SUBSTRING(date::TEXT FROM 1 FOR 7) AS month_year, SUM(total_laid_off)
 FROM layoffs_staging2
@@ -58,7 +58,8 @@ ORDER BY 1 ASC;
 
 
 /*
-	Utilizing a CTE to find the rolling sum of layoffs
+	- Utilizing a CTE and a window function to find the rolling sum of layoffs of each month.
+	- We can use a data visualization to visualize the amount of layoffs in each month and how much the layoffs increases over time.
 */
 WITH Rolling_Sum AS
 (
@@ -68,10 +69,41 @@ WITH Rolling_Sum AS
 	GROUP BY month_year
 	ORDER BY 1 ASC
 )
-SELECT month_year, SUM(years_layoffs) 
+SELECT month_year, years_layoffs, SUM(years_layoffs) OVER(ORDER BY month_year) AS rolling_sum
 FROM Rolling_Sum;
 
 
+/*
+	The rolling sum of the amount of layoffs from each company based on each year.
+*/
+SELECT company, SUBSTRING(date::TEXT FROM 1 FOR 4) AS year, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, year
+HAVING SUM(total_laid_off) IS NOT NULL
+ORDER BY 3 DESC;
+
+
+/*
+	Ranking the amount of layoffs based on the number of total layoffs in each year and the top 5 in each year
+*/
+WITH Company_Year (company, years, total_laid_off) AS
+(
+	SELECT company, SUBSTRING(date::TEXT FROM 1 FOR 4), SUM(total_laid_off)
+	FROM layoffs_staging2
+	GROUP BY company, SUBSTRING(date::TEXT FROM 1 FOR 4)
+	HAVING SUM(total_laid_off) IS NOT NULL
+	ORDER BY 3 DESC
+),
+Company_Year_Rankings AS
+(
+	SELECT *,
+	DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off) AS Ranking
+	FROM layoffs_staging2
+)
+SELECT *, 
+DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off) AS layoff_ranking
+FROM Company_Year
+WHERE years IS NOT NULL;
 
 
 
@@ -79,6 +111,17 @@ FROM Rolling_Sum;
 
 
 
+
+
+
+
+
+
+/*
+	Overall findings of the analysis.
+
+	- 
+*/
 
 
 
